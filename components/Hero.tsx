9ee1115c2155041e145afa38e88a333ac2c46852
Yusuf Kula <yvk2045@gmail.com>
2024-03-29 20:36:21 +0000

    add hero sectionand pre-made css-style, add components file

---
+++ components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title ">
+                Find, book and rent a car - quickly and easily
+            </h1>
+            <p className='hero__subtitle '>
+                Stearmline your car rental
+                experience with our effortless 
+                booking process
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero;
