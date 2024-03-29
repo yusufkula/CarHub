c5eb50e7f0121d64fb871ee1424bfc58c75302c1
Yusuf Kula <yvk2045@gmail.com>
2024-03-29 21:20:26 +0000

    add custom button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+"use client"
 import React from 'react'
+import CustomButton from './CustomButton';
 
 const Hero = () => {
+    const handleScroll = () => {
+
+    }
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -12,6 +17,13 @@ const Hero = () => {
                 experience with our effortless 
                 booking process
             </p>
+            <CustomButton
+                title="Explore Cars"
+                containerStyles="bg-primary-blue 
+                text-white rounded-full mt-10"
+                handleClick={handleScroll}
+
+            />
         </div>
     </div>
   )
