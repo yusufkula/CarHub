ad100676e9da1ea5af047382a051446fc0c7c7ff
Yusuf Kula <yvk2045@gmail.com>
2024-03-30 11:53:02 +0000

    add search manufacturer component and custom filter component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import CustomButton from './CustomButton';
+import Image from 'next/image';
 
 const Hero = () => {
     const handleScroll = () => {
@@ -24,6 +25,15 @@ const Hero = () => {
                 handleClick={handleScroll}
 
             />
+        </div>
+        <div className='hero__image-container'>
+            <div className='hero__image'>
+                <Image src="/hero.png" 
+                alt='hero' 
+                fill className='object-contain'
+                />
+            </div>
+            <div className='hero__image-overlay'/>
         </div>
     </div>
   )
