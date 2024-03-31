7a6c307a12f3a87fc71510a125b886c30f10a54f
Yusuf Kula <yvk2045@gmail.com>
2024-03-31 13:35:59 +0100

    add custom button and finalize the desing of car car component

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import { CarProps } from '@/types'
 import CustomButton from './CustomButton'
 import { calculateCarRent } from '@/utils'
+import CarDetails from './CarDetails'
 
 interface CarCardProps {
     car: CarProps
@@ -13,6 +14,8 @@ interface CarCardProps {
 const CarCard = ({car}: CarCardProps) => {
     const {city_mpg, year, make, model, transmission, drive} = car
     const carRent = calculateCarRent(city_mpg, year)
+
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <div className='car-card group'>
@@ -71,8 +74,18 @@ const CarCard = ({car}: CarCardProps) => {
                         <p className='text-[14px]'>{city_mpg}MPG</p>
                     </div>
                 </div>
-
+                <div className='car-card__btn-container'>
+                    <CustomButton 
+                        title='View More'
+                        containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+                        textStyles = "text-white text-[14px] leading-[17px] font-bold"
+                        rightIcon ="/right-arrow.svg"
+                        handleClick={() => setIsOpen(true)}
+                    />
+                </div>
             </div>
+
+            <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
         </div>
 
 
